30c322045846343777e68df39fe8dafe7275bd09
mhk98 <mohsinkabirseo@gmail.com>
2022-12-08 02:25:48 +0600

    autism backend deploy

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const serverless = require("serverless-http");
 const cors = require("cors");
 const ObjectId = require("mongodb").ObjectId;
 require("dotenv").config();
