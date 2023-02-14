43ad4ab74117c1f463f5b20ea6083a38dd4fd501
mhk98 <mohsinkabirseo@gmail.com>
2023-02-14 23:07:05 +0600

    auti backend done

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
 const cors = require("cors");
-// const ObjectId = require("mongodb").ObjectId;
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
